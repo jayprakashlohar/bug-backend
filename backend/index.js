6479d2c57fc5d32d679e1135264b3257c53bf44c
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-01-19 12:27:42 +0530

    added backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,34 @@
-const express = require("express")
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+require("express-async-errors");
+const { connection } = require("./config/db");
+const { userRouter } = require("./routes/user.route");
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send({ message: "welcome to trendy vibes testing Homepage API" });
+});
+
+app.use("/user", userRouter);
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("db is connected successfully");
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(`Server is listning on PORT${PORT}`);
+});
