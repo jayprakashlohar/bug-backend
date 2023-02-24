09b02971e51a8d2ca58add9340af1d991ab9bd90
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-02-25 00:21:38 +0530

    added

--- Routes/MobileProducts.route.js
+++ Routes/MobileProducts.route.js
@@ -3,10 +3,10 @@ const { AppleProductModel } = require("../Models/MobileProducts.model");
 const mobileRouter = express.Router();
 
 //Get all product
-mobileRouter.get("/", async (req, res) => {
-  const product = await AppleProductModel.find();
-  res.send(product);
-});
+// mobileRouter.get("/", async (req, res) => {
+//   const product = await AppleProductModel.find();
+//   res.send(product);
+// });
 
 //Find by id
 mobileRouter.get("/:id", async (req, res) => {
@@ -14,6 +14,30 @@ mobileRouter.get("/:id", async (req, res) => {
   try {
     const product = await AppleProductModel.findById(id);
     res.status(200).json(product);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//Pagination
+mobileRouter.get("/", async (req, res) => {
+  // const { limit = 10, page = 1 } = req.query;
+  const limit = req.query.limit || 10;
+  const page = req.query.page || 1;
+  try {
+    const data = await AppleProductModel.find({});
+    // .limit(limit)
+    // .skip((page - 1) * limit);
+    let start;
+    let end = page * limit;
+    if (page == 1) {
+      start = page * limit - limit - 1;
+    } else {
+      start = page * limit - limit;
+    }
+
+    let data1 = data.filter((item, ind) => ind >= start && ind < end);
+    res.send(data1);
   } catch (err) {
     res.status(500).send(err.message);
   }
