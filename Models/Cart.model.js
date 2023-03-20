2c806cfdeb619830622d76852ad1c2f23c226958
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-03-20 23:33:46 +0530

    added

--- Models/Cart.model.js
+++ Models/Cart.model.js
@@ -4,7 +4,7 @@ const CartSchema = mongoose.Schema({
   userIds: [],
   title: { type: String, required: true },
   imgUrl: { type: String, required: true },
-  brand: String, 
+  brand: String,
   rate: String,
   price: Number,
   qty: Number,
@@ -13,38 +13,3 @@ const CartSchema = mongoose.Schema({
 const CartModel = mongoose.model("cart", CartSchema);
 
 module.exports = { CartModel };
-
-// const mongoose = require("mongoose");
-
-// const CartSchema = new mongoose.Schema({
-//   userId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User",
-//   },
-//   productId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Product",
-//   },
-//   // name: {
-//   //   type: String,
-//   // },
-//   // price: {
-//   //   type: Number,
-//   // },
-//   // quantity: {
-//   //   type: Number,
-//   // },
-//   // image: {
-//   //   type: String,
-//   // },
-//   title: { type: String },
-//   imgUrl: { type: String },
-//   brand: { type: String },
-//   rate: { type: String },
-//   price: { type: Number },
-//   qty: { type: Number },
-// });
-
-// const CartModel = mongoose.model("Cart", CartSchema);
-
-// module.exports = { CartModel };
